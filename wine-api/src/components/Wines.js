c0df67f0ac837f63858e65fa68fb779eca497223
Teresa Malave <dev.malave7@gmail.com>
2020-04-14 00:52:07 -0500

    got images with name by nesting img element before the closing h2 tag

--- wine-api/src/components/Wines.js
+++ wine-api/src/components/Wines.js
@@ -71,14 +71,15 @@ export default class Wines extends Component {
              <h1>Wine Data Component</h1>
              {/* needed to set a key value to iterate through each unique id to pull names of each wine in data */}
             
-            { this.state.wine.map(details => {
+            { this.state.wine.map(details => 
                 
-                <div>
-                <h2 key= { details.id }>{ details.name } </h2>
-
-                <img key= { details.id } src={ details.picture } alt= { details.name }></img>
-                </div>
-            })}
+                
+                <h2 key= { details.id }> { details.name } 
+                
+                <img src= { details.picture } alt= ' '/>
+                </h2>
+                
+            )}
          
 
 
