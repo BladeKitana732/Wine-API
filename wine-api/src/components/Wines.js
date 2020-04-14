7c557fe119547f956fe539f3ee9e67e172b2f6f9
Teresa Malave <dev.malave7@gmail.com>
2020-04-14 00:07:57 -0500

    commenting display code

--- wine-api/src/components/Wines.js
+++ wine-api/src/components/Wines.js
@@ -69,9 +69,12 @@ export default class Wines extends Component {
         return (
             <div>
              <h1>Wine Data Component</h1>
-
+             {/* needed to set a key value to iterate through each unique id to pull names of each wine in data */}
              { this.state.wine.map(details => <h2 key={details.id}>{details.name}</h2>)}
+
+
             </div>
+
         )
     }
 }
