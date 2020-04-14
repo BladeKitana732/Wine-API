d63fa329ec5e25cfd75c1259cf5bcaf85ed52af3
Teresa Malave <dev.malave7@gmail.com>
2020-04-13 23:54:08 -0500

    testing new information from resources to try and display wine info

--- wine-api/src/components/Wines.js
+++ wine-api/src/components/Wines.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-//render wine data 
+//render wine data - completed in console 
+//resource found: https://www.youtube.com/watch?v=12l6lkW6JhE&t=564s
+//resource found: https://www.codota.com/code/javascript/functions/axios/create
+
+//creating a api variable as base to be able to pull information from the separate objects in unison
+
+let wineLink = axios.create({
+    baseURL: "http://myapi-profstream.herokuapp.com/api/2f49a6/wines"
+})
 
 
 export default class Wines extends Component {
+
+
+    constructor(props) {
+        super(props);
+
+
+    }
 
     state = {
         
@@ -12,33 +27,29 @@ export default class Wines extends Component {
 
     }
 
-    componentDidMount() {
-        
-        let wineLink = "http://myapi-profstream.herokuapp.com/api/2f49a6/wines";
+    // componentDidMount() {
 
-        axios.get(wineLink)
+    //     axios.get(wineLink)
     
-        .then((result) => {
-            const wines = result.data;
+    //     .then((result) => {
+    //         const wines = result.data;
 
-
+    //         console.log(wines);
     
-            console.log(wines[0]);
-    
-            this.setState({
-                wine1: wines[0]
-            });
+    //         this.setState({
+    //             wine: result.data
+    //         });
             
-        })
+    //     })
 
-        .catch((err) => {
+    //     .catch((err) => {
 
-            console.log('Error has been computed', err)
+    //         console.log('Error has been computed', err)
     
-        });
+    //     });
 
 
-    }
+    // }
 
 
     render() {
