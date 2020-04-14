bd5711f976f784a84c247622aae584f4792f3db6
Teresa Malave <dev.malave7@gmail.com>
2020-04-13 20:49:18 -0500

    adding axios and component for wines

---
+++ wine-api/src/components/Wines.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Wines extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
